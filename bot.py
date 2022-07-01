b32a4b3030d321466051c17d15a7d9165dfaac5d
bulldozerlk <106230278+bulldozerlk@users.noreply.github.com>
2022-07-01 14:58:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from pyrogram import Client
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-
-API_ID = int(os.environ.get("API_ID", ""))
-
-API_HASH = os.environ.get("API_HASH", "")
+TOKEN = os.environ.get("TOKEN", "5584644386:AAEXiEOU3e1tiHWYq8OOKW9mU2g_iMQKjMY")
+ADMIN = "5363862546"
+API_ID = int(os.environ.get("API_ID", "18497514"))
+DB_URL = "mongodb+srv://990909jjjjfdrew4rfrfdrdsese4567890moko:990909jjjjfdrew4rfrfdrdsese4567890moko@cluster0.lkqip.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "fk"
+API_HASH = os.environ.get("API_HASH", "d09d122ec6dce14a421e177b88bc1293")
 
 if __name__ == "__main__" :
     plugins = dict(
